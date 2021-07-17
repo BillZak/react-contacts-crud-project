5fa7ee22e2fe8ada11527e40fa0879e0f1228149
Bilal Zak <bzak954@gmail.com>
2021-07-17 18:25:45 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Contact from './components/Contact';
+
 import ContactForm from './components/ContactForm'
 
 import React, { Component } from 'react';
+import ContactList from './components/ContactList';
 
 class App extends Component {
   constructor(props){
@@ -9,26 +10,32 @@ class App extends Component {
     this.state ={ contacts :[]}
   }
   handleAddContact = (newContact) => {
+    newContact.id = Math.random().toString();
     this.setState({ 
         contacts: [...this.state.contacts,newContact] 
     })
   }
 
+  handleDeleteContact = (contactId) =>{
+     const savedContacts = this.state.contacts.filter(contact => contact.id !== contactId);
+
+     this.setState({contacts: savedContacts})
+  }
+
+  handleEditContact = (updatedContact) =>{
+    this.setState({
+      contacts: this.state.contacts.map(contact => contact.id === updatedContact.id ? updatedContact : contact),
+    })
+  };
+
 
   render() {
-   
-    const contacts = this.state.contacts.map((contacts,index) =>{
-            return(
-              <Contact contact={contacts} index={index}/>
-            )
-    })
-
-
-
+  
     return (
       <>
          <ContactForm addContact={this.handleAddContact}/>
-         {contacts}
+         <ContactList contacts= {this.state.contacts} deleteContact= {this.handleDeleteContact}
+          editContact={this.handleEditContact}/>
       </>
     );
   }
