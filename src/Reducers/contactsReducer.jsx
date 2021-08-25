611c3ea673a0b2e166f7be7137cde3bd7e410058
Bill Zack <bzak954@gmail.com>
2021-08-25 13:18:07 +0000

    add reducer functions

--- src/Reducers/contactsReducer.jsx
+++ src/Reducers/contactsReducer.jsx
@@ -6,7 +6,15 @@ const contactsReducer = ( state = initialState ,action) =>{
     switch(action.type){
         case "ADD_CONTACT":
         return {...state.contacts, contacts: [...state.contacts, action.payload]};
-
+        case "DELETE_CONTACT":
+            const savedContacts = state.contacts.filter(
+                (contact) => {
+                return contact.id !== action.payload;
+            })
+            return {...state,contacts: savedContacts}
+        case "EDIT_CONTACT":
+        return {...state,contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact),
+        }
         default: return state;
     }
 }
