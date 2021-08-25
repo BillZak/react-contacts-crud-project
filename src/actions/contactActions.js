b92b2257743ba54eb20fae58020c98c008419438
Bill Zack <bzak954@gmail.com>
2021-08-25 13:16:51 +0000

    add actions

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,6 +1,21 @@
 export const addContact = ( newContact )=>{
+    newContact.id = Math.random().toString();
     return{
         type: "ADD_CONTACT",
         payload: newContact,
+    }
+}
+
+export const deleteContact = ( contactId ) =>{
+    return{
+        type: "DELETE_CONTACT",
+        payload: contactId
+    }
+}
+
+export const editContact = ( updatedUser ) =>{
+    return{
+        type: "EDIT_CONTACT",
+        payload: updatedUser
     }
 }
