966cd1a64c63c37d6b83e7ca1d43371e679bc054
Bill Zack <bzak954@gmail.com>
2021-08-25 13:16:37 +0000

    remove functions

--- src/App.js
+++ src/App.js
@@ -6,27 +6,6 @@ import ContactList from './components/ContactList';
 
 class App extends Component {
 
-
-  handleAddContact = (newContact) => {
-    newContact.id = Math.random().toString();
-    this.setState({ 
-        contacts: [...this.state.contacts,newContact] 
-    })
-  }
-
-  handleDeleteContact = (contactId) =>{
-     const savedContacts = this.state.contacts.filter(contact => contact.id !== contactId);
-
-     this.setState({contacts: savedContacts})
-  }
-
-  handleEditContact = (updatedContact) =>{
-    this.setState({
-      contacts: this.state.contacts.map(contact => contact.id === updatedContact.id ? updatedContact : contact),
-    })
-  };
-
-
   render() {
   
     return (
