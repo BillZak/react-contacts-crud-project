099b246ad1d7a002143fc09c84bd863cc2b62f1c
Bill Zack <bzak954@gmail.com>
2021-09-09 12:44:47 +0000

    add function for middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,14 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import contactsReducer from "../Reducers/contactsReducer";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import  thunk  from "redux-thunk"
+import firebase from '../firebase/config'
 
-const store = createStore(contactsReducer)
+const store = createStore(contactsReducer , compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(firebase),
+    reduxFirestore(getFirestore)
+))
 
 export default store;
