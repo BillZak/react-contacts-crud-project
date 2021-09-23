6f4ed77bb7a2e6e978a90c013d0b055d33d18448
Bill Zack <bzak954@gmail.com>
2021-09-23 12:57:13 +0000

    add setallcontacts case

--- src/Reducers/contactsReducer.jsx
+++ src/Reducers/contactsReducer.jsx
@@ -15,6 +15,9 @@ const contactsReducer = ( state = initialState ,action) =>{
         case "EDIT_CONTACT":
         return {...state,contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact),
         }
+        case "SET_ALL_CONTACTS":
+        return {...state,contacts:action.payload}
+
         default: return state;
     }
 }
