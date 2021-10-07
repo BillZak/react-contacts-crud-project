10a39f60311831fd3e63841829ef0911853e0a6a
Bill Zack <bzak954@gmail.com>
2021-10-07 17:18:09 +0000

    add home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ContactList from '../components/ContactList'
+import ContactForm from '../components/ContactForm'
+
+
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <ContactForm />
+                <h1 style={{fontSize:'40px',marginLeft:'500px',marginTop:'50px'}}>Added Contacts</h1>
+            <ContactList />
+        </>
     );
 }
 
 export default Home;
+
